983ae0928905573cc450cd0be48c611d55fc2c90
Joel Luis Steffen <jo.lsteffen@gmail.com>
2023-12-20 09:23:48 -0300

    trabalhando

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const cards = document.getElementById
+const cards = document.getElementById("cards");
 
 const instance = axios.create({
   baseURL: "https://rickandmortyapi.com/api",
@@ -6,13 +6,33 @@ const instance = axios.create({
 
 async function selecionarPersonagem(event) {
   event.preventDefault();
+  const personagens = [];
+  cards.innerHTML = "";
   let busca = parseInt(event.srcElement.buscador.value);
-  
+
   if (isNaN(busca)) {
-    busca = Math.ceil(Math.random()*826)
+    busca = Math.ceil(Math.random() * 826);
   }
 
-  return(console.log(busca))
+  let url = `/character/${busca}`;
+
+  const resposta = await instance.get(url);
+  personagens.push(resposta.data);
+
+  personagens.forEach((personagem) => {
+    cards.innerHTML += `<article>
+    <figure>
+      <img src=${personagem.image} alt="${personagem.name}." />
+    </figure>
+    <aside>
+    <p><h3>ID:</h3>${personagem.id}.</p>
+    <p><h3>Name:</h3>${personagem.name}.</p>
+    <p><h3>Status:</h3>${personagem.status}.</p>
+    <p><h3>Species:</h3>${personagem.species}.</p>
+    <p><h3>Location:</h3>${personagem.location.name}.</p>
+    </aside>
+  </article>`;
+  });
 }
 
 async function carregaPersonagens(pagina) {
@@ -29,16 +49,26 @@ async function carregaPersonagens(pagina) {
   const resposta = await instance.get(url);
   const personagens = resposta.data.results;
 
-  //organizapersonagens (personagens);
-  return(console.log("Funcionando até aqui " + pagina))
+  organizapersonagens(personagens);
+  console.log(personagens);
 }
 
-/*
-function organizapersonagens (personagens) {
-  personagens.array.forEach(personagem => {
-    
+function organizapersonagens(personagens) {
+  cards.innerHTML = "";
+  personagens.forEach((personagem) => {
+    cards.innerHTML += `<article>
+    <figure>
+      <img src=${personagem.image} alt="${personagem.name}." />
+    </figure>
+    <aside>
+    <p><h3>ID:</h3>${personagem.id}.</p>
+    <p><h3>Name:</h3>${personagem.name}.</p>
+    <p><h3>Status:</h3>${personagem.status}.</p>
+    <p><h3>Species:</h3>${personagem.species}.</p>
+    <p><h3>Location:</h3>${personagem.location.name}.</p>
+    </aside>
+  </article>`;
   });
 
+  console.log(personagens);
 }
-
-*/
